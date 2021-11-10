4b41be19efeff56843df1aa66adbf9e76059589f
Andreas Haller <halleritis@gmail.com>
2021-11-10 22:14:18 +0100

    added measure task (skip maybe)

--- measure.jl
+++ measure.jl
@@ -7,20 +7,20 @@ function measure_obs(AC)
     Sz = 0.5 .* [1    0;   0 -1]
 
     # nearest neighbor correlations
-    @tensor xx[-1,-2,-3,-4] := Sx[-1,-3]*Sx[-2,-4]
-    @tensor yy[-1,-2,-3,-4] := Sy[-1,-3]*Sy[-2,-4]
-    @tensor zz[-1,-2,-3,-4] := Sz[-1,-3]*Sz[-2,-4]
-    xx_exp = @tensor AC[1 2 3 4]*xx[2 3 5 6]*conj(AC[1 5 6 4])
-    yy_exp = @tensor AC[1 2 3 4]*yy[2 3 5 6]*conj(AC[1 5 6 4])
-    zz_exp = @tensor AC[1 2 3 4]*zz[2 3 5 6]*conj(AC[1 5 6 4])
+    @tensor xx[] := Sx[]*Sx[]
+    @tensor yy[] := Sy[]*Sy[]
+    @tensor zz[] := Sz[]*Sz[]
+    xx_exp = @tensor AC[]*xx[]*conj(AC[])
+    yy_exp = @tensor AC[]*yy[]*conj(AC[])
+    zz_exp = @tensor AC[]*zz[]*conj(AC[])
 
     # average magnetization
-    @tensor mx[-1,-2,-3,-4] := 0.5*Sx[-1,-3]*S0[-2,-4] + 0.5*S0[-1,-3]*Sx[-2,-4]
-    @tensor my[-1,-2,-3,-4] := 0.5*Sy[-1,-3]*S0[-2,-4] + 0.5*S0[-1,-3]*Sy[-2,-4]
-    @tensor mz[-1,-2,-3,-4] := 0.5*Sz[-1,-3]*S0[-2,-4] + 0.5*S0[-1,-3]*Sz[-2,-4]
-    mx_exp = @tensor AC[1 2 3 4]*mx[2 3 5 6]*conj(AC[1 5 6 4])
-    my_exp = @tensor AC[1 2 3 4]*my[2 3 5 6]*conj(AC[1 5 6 4])
-    mz_exp = @tensor AC[1 2 3 4]*mz[2 3 5 6]*conj(AC[1 5 6 4])
+    @tensor mx[] := 0.5*Sx[]*S0[] + 0.5*S0[]*Sx[]
+    @tensor my[] := 0.5*Sy[]*S0[] + 0.5*S0[]*Sy[]
+    @tensor mz[] := 0.5*Sz[]*S0[] + 0.5*S0[]*Sz[]
+    mx_exp = @tensor AC[]*mx[]*conj(AC[])
+    my_exp = @tensor AC[]*my[]*conj(AC[])
+    mz_exp = @tensor AC[]*mz[]*conj(AC[])
 
     # return the two observables in two arrays
     return [xx_exp, yy_exp, zz_exp], [mx_exp, my_exp, mz_exp]
