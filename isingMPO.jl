a2e4149a430df65dca44c446abe2ff9b9b45b1a8
Andreas Haller <halleritis@gmail.com>
2021-11-10 17:41:28 +0100

    renamed variable

--- isingMPO.jl
+++ isingMPO.jl
@@ -39,15 +39,15 @@ let  # let environment to avoid global scope...
     σ₀ = Matrix(I, 2, 2)
 
     J=1; h=2
-    lvec, W, rvec = isingMPO(J=J, h=h)
+    L, W, R = isingMPO(J=J, h=h)
 
     ⊗(A,B) = kron(A,B)
-    @tensor LWWR[:] := lvec[4,1]*W[1,-1,2,-3]*W[2,-2,3,-4]*rvec[3,4]
+    @tensor LWWR[:] := L[4,1]*W[1,-1,2,-3]*W[2,-2,3,-4]*R[3,4]
     @show J*(σ₁⊗σ₁)+h*(σ₃⊗σ₀+σ₀⊗σ₃) ≈ reshape(LWWR, (4,4))
 
-    @tensor LWWWR[:] := lvec[5,1]*W[1,-4,2,-1]*W[2,-5,3,-2]*W[3,-6,4,-3]*rvec[4,5]
+    @tensor LWWWR[:] := L[5,1]*W[1,-4,2,-1]*W[2,-5,3,-2]*W[3,-6,4,-3]*R[4,5]
     @show J*(σ₁⊗σ₁⊗σ₀+σ₀⊗σ₁⊗σ₁)+h*(σ₃⊗σ₀⊗σ₀+σ₀⊗σ₃⊗σ₀+σ₀⊗σ₀⊗σ₃) ≈ reshape(LWWWR, (8,8))
 
-    @tensor LWWWWR[:] := lvec[6,1]*W[1,-5,2,-1]*W[2,-6,3,-2]*W[3,-7,4,-3]*W[4,-8,5,-4]*rvec[5,6]
+    @tensor LWWWWR[:] := L[6,1]*W[1,-5,2,-1]*W[2,-6,3,-2]*W[3,-7,4,-3]*W[4,-8,5,-4]*R[5,6]
     @show J*(σ₁⊗σ₁⊗σ₀⊗σ₀+σ₀⊗σ₁⊗σ₁⊗σ₀+σ₀⊗σ₀⊗σ₁⊗σ₁)+h*(σ₃⊗σ₀⊗σ₀⊗σ₀+σ₀⊗σ₃⊗σ₀⊗σ₀+σ₀⊗σ₀⊗σ₃⊗σ₀+σ₀⊗σ₀⊗σ₀⊗σ₃) ≈ reshape(LWWWWR, (16,16))
 end
